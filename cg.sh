cb7bcdf5e6d3e6297d71a51fd032dd4f73ec5f4b
brandon <brandon@gmail.com>
2022-03-27 19:54:57 +0700

    mod

--- cg.sh
+++ cg.sh
@@ -5,12 +5,12 @@ git pull origin main
 git submodule init
 git submodule update
 
-PROJECT="reportApi2"
+PROJECT="reportapi"
 GitReversion=`git rev-parse HEAD`
 BuildTime=`date +'%Y.%m.%d.%H%M%S'`
 BuildGoVersion=`go version`
 
 go build -ldflags "-X main.gitReversion=${GitReversion}  -X 'main.buildTime=${BuildTime}' -X 'main.buildGoVersion=${BuildGoVersion}'" -o $PROJECT
 # bigbet88
-sshpass -p $1 scp $PROJECT root@192.168.80.220:/home/centos/workspace/egame/$PROJECT
+sshpass -p $1 scp $PROJECT testp3@34.92.240.177:/home/centos/cg/$PROJECT
 
