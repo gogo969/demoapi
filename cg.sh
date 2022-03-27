33b081e0192c0182abe7230d5960849e77754cc9
brandon <brandon@gmail.com>
2022-03-27 20:05:10 +0700

    fix

--- cg.sh
+++ cg.sh
@@ -11,6 +11,6 @@ BuildTime=`date +'%Y.%m.%d.%H%M%S'`
 BuildGoVersion=`go version`
 
 go build -ldflags "-X main.gitReversion=${GitReversion}  -X 'main.buildTime=${BuildTime}' -X 'main.buildGoVersion=${BuildGoVersion}'" -o $PROJECT
-# bigbet88
-sshpass -p $1 scp $PROJECT testp3@34.92.240.177:/home/centos/cg/$PROJECT
+# cg
+sshpass -p $1 scp $PROJECT testp3@34.92.240.177:/home/centos/workspace/cg/$PROJECT
 
