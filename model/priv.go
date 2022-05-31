56e58c29a8f7b3e344e2f4bf36b64c8cadfe93d2
brandon <brandon@gmail.com>
2022-05-31 22:18:39 +0700

    权限

--- model/priv.go
+++ model/priv.go
@@ -7,12 +7,13 @@ import (
 
 func PrivCheck(uri, gid string) error {
 
-	privId, err := meta.MerchantRedis.HGet(ctx, "PrivMap", uri).Result()
+	privMapKey := fmt.Sprintf("%s:priv:PrivMap", meta.Prefix)
+	privId, err := meta.MerchantRedis.HGet(ctx, privMapKey, uri).Result()
 	if err != nil {
 		return err
 	}
 
-	id := fmt.Sprintf("GM%s", gid)
+	id := fmt.Sprintf("%s:priv:GM%s", meta.Prefix, gid)
 	exists := meta.MerchantRedis.HExists(ctx, id, privId).Val()
 	if !exists {
 		return errors.New("404")
